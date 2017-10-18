8e05d3dc8e526f733280ee930ae41a06610617cc
marynganga <marynganga.mw@gmail.com>
2017-10-18 14:23:52 +0300

    Setup the movie database API key

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from .config import DevConfig
 
 # initialize application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
 
 # setting up configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from app import views
--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
 	'''
     General configuration parent class
     '''
-    pass
+    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
 
 class ProdConfig(Config):
 	'''

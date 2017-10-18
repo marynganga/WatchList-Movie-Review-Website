67b73ed2fea8c936fad007f9d1e9b453fc2c0177
marynganga <marynganga.mw@gmail.com>
2017-10-18 15:25:33 +0300

    Create a function to process API request results

--- app/request.py
+++ app/request.py
@@ -14,3 +14,41 @@ def get_movies(category):
     Function that gets the json response to our url request
     '''
     get_movies_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_movies_url) as url:
+    	get_movies_data = url.read()
+    	get_movies_response = json.loads(get_movies_data)
+    	movie_results = None
+
+    	if get_movies_response['results']:
+    		movie_results_list = get_movies_response['results']
+    		movie_results = process_results(movie_results_list)
+
+    		return movie_results
+
+def process_results(movie_results):
+	'''
+    Function  that processes the movie result and transform them to a list of Objects
+
+    Args:
+        movie_list: A list of dictionaries that contain movie details
+
+    Returns :
+        movie_results: A list of movie objects
+    '''
+    movie_list = []
+    for movie_item in movie_results:
+    	id = movie_item.get('id')
+        title = movie_item.get('original_title')
+        overview = movie_item.get('overview')
+        poster = movie_item.get('poster_path')
+        vote_average = movie_item.get('vote_average')
+        vote_count = movie_item.get('vote_count')
+
+        if poster:
+        	movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+            movie_list.append(movie_object)
+
+        return movie_list
+
+

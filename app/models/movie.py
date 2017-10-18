3f8ac3f7ec0fd5769e4fc61ab0a3bc964238dc3b
marynganga <marynganga.mw@gmail.com>
2017-10-18 14:48:36 +0300

    Create a model class for a Movie and a test file

---
+++ app/models/movie.py
@@ -0,0 +1,20 @@
+class Movie:
+	'''
+    Movie class to define Movie Objects
+    '''
+	def __init__(self,id,title,overview,image,vote_average,vote_count):
+		'''
+		Args: 
+			1. Title - The name of the movie
+			2. Overview - A short description on the movie
+			3. image- The poster image for the movie
+			4. vote_average - Average rating of the movie
+			5. vote_count - How many people have rated the movie
+			6. id - The movie id
+		'''
+		self.id =id
+		self.title = title
+		self.overview = overview
+		self.image = 'https://developers.themoviedb.org/3/getting-started/images'+image
+		self.vote_average = vote_average
+		self.vote_count = vote_count

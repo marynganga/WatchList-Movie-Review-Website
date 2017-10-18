34ff6f1a48e6e311c5e75ffbd4b7af07c33a3ae3
marynganga <marynganga.mw@gmail.com>
2017-10-18 19:10:56 +0300

    Add links to the movie details page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .request import get_movies, get_movie
 
 #Views
 @app.route('/')
@@ -16,9 +16,11 @@ def index():
 	title = 'Home - Welcome to The best Movie Review Website Online'
 	return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movies, now_showing = now_showing_movies)
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+@app.route('/movie/<int:id>')
+def movie(id):
 	'''
 	View movie page function that returns the movie details page and its data
 	'''
-	return render_template('movie.html',id=movie_id)
+	movie = get_movie(id)
+	title = f'{movie.title}'
+	return render_template('movie.html',title=title,movie=movie)

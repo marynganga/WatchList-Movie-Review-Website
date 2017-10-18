de0d389f5a4c26789f21b7e08c2bffa578c79770
marynganga <marynganga.mw@gmail.com>
2017-10-18 14:03:09 +0300

    Create a root and view for the movie page

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .config import DevConfig
 
 # initialize application
 app = Flask(__name__)
+
+# setting up configuration
+app.config.from_object(DevConfig)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -7,5 +7,12 @@ def index():
 	'''
 	View root page function that returns the index page and its data
 	'''
-	message = 'Hello World'
-	return render_template('index.html',message=message)
+	title = 'Home - Welcome to The best Movie Review Website Online'
+	return render_template('index.html',title=title)
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+	'''
+	View movie page function that returns the movie details page and its data
+	'''
+	return render_template('movie.html',id=movie_id)

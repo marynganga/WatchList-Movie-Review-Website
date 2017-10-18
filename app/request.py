8edbec8c5d698e0726eeb08d75f71dccc8e03db8
marynganga <marynganga.mw@gmail.com>
2017-10-18 15:14:19 +0300

    Create an API call function

--- app/request.py
+++ app/request.py
@@ -1,4 +1,16 @@
 from app import app 
+import urllib.request,json
+from models import movie
+Movie = movie.Movie
 
 # Getting api key
 api_key = app.config['MOVIE_API_KEY']
+
+# Getting the movie base url
+base_url = app.config['MOVIE_API_KEY_BASE_URL']
+
+def get_movies(category):
+	'''
+    Function that gets the json response to our url request
+    '''
+    get_movies_url = base_url.format(category,api_key)

10a1a2c1134676daf51a4eb875a4455b94652a9b
marynganga <marynganga.mw@gmail.com>
2017-10-23 14:47:16 +0300

    Create a i=one to many relationship with the user and role models. 1. Add a foreign key in the user model. 2. Add a db.relationship instance in the role model.

--- app/models.py
+++ app/models.py
@@ -55,7 +55,7 @@ class User(db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.Integer,primary_key = True)
 	username = db.Column(db.String(255))
-
+	role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 	def __repr__(self):
 		return f'User {self.username}'
 
@@ -63,7 +63,6 @@ class Role(db.Model):
 	__tablename__ = 'roles'
 	id = db.Column(db.Integer,primary_key=True)
 	name = db.Column(db.String(255))
-
+	users = db.relationship('User',backref = 'role', lazy = 'dynamic')
 	def __repr__(self):
 		return f'User {self.name}'
-		

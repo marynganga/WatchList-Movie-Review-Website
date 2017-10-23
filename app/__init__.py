c6caf20c9d096f28422340ce9ccf09260a7cdba5
marynganga <marynganga.mw@gmail.com>
2017-10-23 17:55:45 +0300

    Import and initialize flask_login module

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from config import config_options
 
 # Creating Flask Extensions Instances
@@ -17,11 +18,18 @@ def create_app(config_name):
 	# initializing flask extensions
 	bootstrap.init_app(app)
 	db.init_app(app)
+	login_manager = LoginManager()
+	login_manager.session_protection = 'strong'
+	login_manager.login_view = 'auth.login'
+
 
 	# registering the blueprint
 	from .main import main as main_blueprint
+	from .auth import auth as auth_blueprint
+
 
 	app.register_blueprint(main_blueprint)
+	app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
 
 	# setting config
 	from .request import configure_request

10a851834bedbe05fef844502dc683fa231e4aed
marynganga <marynganga.mw@gmail.com>
2017-10-23 16:55:33 +0300

    1. Create a new column to hold User passwords. 2. Create a new migration and upgrade to it. 3. Create functions for user authentication.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Movie:
 	'''
@@ -56,6 +57,20 @@ class User(db.Model):
 	id = db.Column(db.Integer,primary_key = True)
 	username = db.Column(db.String(255))
 	role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+	pass_secure = db.Column(db.String(255))
+
+	@property
+	def password(self):
+		raise AttributeError('You cannot read the password attribute.')
+
+	@password.setter
+	def password(self, password):
+		self.pass_secure = generate_password_hash(password)
+
+	def verify_pasword(self,password):
+		return check_password_hash(self.pass_secure,password)
+
+
 	def __repr__(self):
 		return f'User {self.username}'
 

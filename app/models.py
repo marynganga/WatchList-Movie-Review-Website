c32e086e05f0ee6ebef4c6f0677358bacf0b4ab0
marynganga <marynganga.mw@gmail.com>
2017-10-23 14:37:54 +0300

    Create a model for user Roles.

--- app/models.py
+++ app/models.py
@@ -58,3 +58,12 @@ class User(db.Model):
 
 	def __repr__(self):
 		return f'User {self.username}'
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer,primary_key=True)
+	name = db.Column(db.String(255))
+
+	def __repr__(self):
+		return f'User {self.name}'
+		

13bdc88cd257600e222e4d53cbcfb5bb3799117b
marynganga <marynganga.mw@gmail.com>
2017-10-23 16:29:53 +0300

    Import the flask migrate module in manage.py and initialize it.

--- manage.py
+++ manage.py
@@ -1,11 +1,15 @@
 from app import create_app,db
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 from app.models import User,Role
 # Creating the app instance
 app = create_app('development')
 
+
+migrate = Migrate(app,db)
 manager = Manager(app)
 manager.add_command('server',Server)
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
